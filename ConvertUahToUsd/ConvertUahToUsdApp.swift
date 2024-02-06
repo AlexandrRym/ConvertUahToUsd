42d4b3c2529a8a9793e6c55a8f81d3c70ed0bc21
Alex Rymar <lostmannight@gmail.com>
2024-02-05 19:13:57 -0800

    Initial Commit

---
+++ ConvertUahToUsd/ConvertUahToUsdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertUahToUsdApp.swift
+//  ConvertUahToUsd
+//
+//  Created by AlexRymar on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ConvertUahToUsdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
